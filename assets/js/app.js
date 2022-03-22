54965413a6c2fcaa610af03d2d6228f9050447fe
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-03-22 15:26:46 +0100

    working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 let game = document.querySelector(".game")
+let controller = document.querySelector(".game__controller")
 let ball = document.querySelector(".game__ball")
 let gameHeight = game.getBoundingClientRect().height
 
@@ -16,11 +17,21 @@ for (let i = 0; i < 100; i++) {
     generateBlock()
 }
 
+// Stockage de la position des blocs
+let positionBlocs = []
+for (let i = 2; i < game.children.length; i++) {
+    let position = []
+    position.push(game.children[i].getBoundingClientRect().x, game.children[i].getBoundingClientRect().y)
+    positionBlocs.push(position)
+}
+
 // Position de départ
-let ballLeft = ball.style.left = 720/2 - 15 + "px"
-let ballBottom = ball.style.bottom = "10px"
+let ballLeft = game.getBoundingClientRect().width/2 - 8 + "px"
+ball.style.left  = ballLeft
+let ballBottom = "30px"
+ball.style.bottom = ballBottom
 // Vitesse du jeu
-let speed = 2
+let speed = 1
 let xSpeed = speed
 let ySpeed = speed
 
@@ -30,22 +41,57 @@ let moveBall = function() {
     ballBottom = parseInt(ballBottom.split("px")[0]) + ySpeed + "px"
     ball.style.bottom = ballBottom
 
+    // La balle reste dans la fenêtre de jeu
     if (parseInt(ballBottom.split("px")[0]) > game.getBoundingClientRect().height - 38) {
         ySpeed = -speed
-        console.log("1")
     }
     if (parseInt(ballBottom.split("px")[0]) < 0) {
-        ySpeed = speed
-        console.log("2")
+        clearInterval(interval)
     }
     if (parseInt(ballLeft.split("px")[0]) > game.getBoundingClientRect().width - 38) {
         xSpeed = -speed
-        console.log("3")
     }
     if (parseInt(ballLeft.split("px")[0]) < 0) {
         xSpeed = speed
-        console.log("4")
+    }
+
+    // La balle rebondit sur le controller
+    if (parseInt(ballBottom.split("px")[0]) < parseInt(controllerBottom.split("px")[0]) + 18 && parseInt(controllerLeft.split("px")[0]) < parseInt(ballLeft.split("px")[0]) && parseInt(ballLeft.split("px")[0]) < (parseInt(controllerLeft.split("px")[0])) + 250) {
+        ySpeed = speed
+    }
+
+    // La balle détruit les blocs
+    let positionBall = []
+    positionBall.push(ball.getBoundingClientRect().x, ball.getBoundingClientRect().y)
+    for (let i = 2; i < game.children.length; i++) {
+        if ((positionBlocs[i-2][0] - positionBall[0] > -20 && positionBlocs[i-2][0] - positionBall[0] < 20) && (positionBlocs[i-2][1] - positionBall[1] > -20 && positionBlocs[i-2][1] - positionBall[1] < 20) && game.children[i].style.opacity !== "0") {
+            game.children[i].style.opacity = "0"
+            ySpeed = -speed
+            // Activation des bonus
+            if (game.children[i].classList) {
+                
+            }
+        }
     }
 }
 
-setInterval(moveBall, 1)
+// Controller
+let controllerLeft = game.getBoundingClientRect().width/2 - 125 + "px"
+controller.style.left = controllerLeft
+let controllerBottom = "10px"
+controller.style.bottom = controllerBottom
+
+let interval
+document.addEventListener("keydown", function(e) {
+    if ((e.key === "ArrowLeft" || e.key === "ArrowRight") && interval === undefined) {
+        interval = setInterval(moveBall, 1)
+    }
+    if (e.key === "ArrowLeft" && parseInt(controller.style.left.split("px")[0]) > 15) {
+        controllerLeft = parseInt(controller.style.left.split("px")[0]) - 20 + "px"
+        controller.style.left = controllerLeft
+    }
+    if (e.key === "ArrowRight" && parseInt(controller.style.left.split("px")[0]) < 455) {
+        controllerLeft = parseInt(controller.style.left.split("px")[0]) + 20 + "px"
+        controller.style.left = controllerLeft
+    }
+})
